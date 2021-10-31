b0b20d2dd5cbaa03dbe4d2be5d120ce87d652ab5
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-10-31 23:23:32 +0100

    Update write_downloader_ini.py

--- legacy/write_downloader_ini.py
+++ legacy/write_downloader_ini.py
@@ -26,7 +26,7 @@ def main():
     db_url_mister_db9_distribution_mister = 'https://raw.githubusercontent.com/MiSTer-DB9/Distribution_MiSTer/main/dbencc.json.zip'
     db_id_distribution_mister = 'distribution_mister'
 
-    db_url_jtbin_jtcores = 'https://raw.githubusercontent.com/theypsilon/JT_Cores_MiSTer/jtbin/jtbindb.json.zip'
+    db_url_jtbin_jtcores = 'https://raw.githubusercontent.com/jotego/jtcores_mister/main/jtbindb.json.zip'
     db_url_jtstable_jtcores = 'https://raw.githubusercontent.com/theypsilon/JT_Cores_MiSTer/jtstable/jtstabledb.json.zip'
     db_id_jtcores = 'jtcores'
 
