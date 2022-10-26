b8103bd59e0d4eb21738d146c979c4af1f273fb7
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-10-27 01:16:45 +0200

    Adding insecure launcher.

---
+++ update_all_insecure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2022 José Manuel Barroso Galindo <theypsilon@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# You can download the latest version of this tool from:
+# https://github.com/theypsilon/Update_All_MiSTer
+
+set -euo pipefail
+
+LAUNCHER="$(dirname ${BASH_SOURCE[0]})/update_all.sh"
+
+export CURL_SSL="--insecure"
+
+${LAUNCHER}
