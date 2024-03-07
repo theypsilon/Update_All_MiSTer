ee740fddc9bc62050677e753df6d7d97092f9510
José Manuel Barroso Galindo <theypsilon@gmail.com>
2024-03-07 19:07:03 +0100

    Pushing update_all_mister db

--- .github/release.sh
+++ .github/release.sh
@@ -3,6 +3,18 @@
 
 import subprocess
 import os
+import time
+import json
+import hashlib
+
+def hash_file(path: str) -> str:
+    with open(path, "rb") as f:
+        file_hash = hashlib.md5()
+        chunk = f.read(8192)
+        while chunk:
+            file_hash.update(chunk)
+            chunk = f.read(8192)
+        return file_hash.hexdigest()
 
 subprocess.run(['git', 'add', 'dont_download2.sh'], check=True)
 subprocess.run(['git', 'commit', '-m', 'BOT: New dont_download2.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -16,7 +28,36 @@ if int(changes) >= 1:
     print("There are changes to push:")
     print(changes)
     print()
+
+    subprocess.run(['git', 'checkout', '--orphan', 'db'], check=True)
+    subprocess.run(['git', 'rm', '-rf', '.'], check=False)
+    subprocess.run(['git', 'add', 'dont_download2.sh'], check=True)
+    subprocess.run(['git', 'commit', '-m', '-'], check=True)
+
+    commit_id = subprocess.getoutput("git rev-parse HEAD")
+
+    db = {
+        "db_id": 'update_all_mister',
+        "files": {
+            'Scripts/.config/update_all/update_all_latest.zip': {
+                'size': os.path.getsize('dont_download2.sh'),
+                'hash': hash_file('dont_download2.sh'),
+                'url': f'https://raw.githubusercontent.com/theypsilon/Update_All_MiSTer/{commit_id}/dont_download2.sh',
+                'tags': ['updatealllatest', 'updateall']
+            }
+        },
+        "folders": {'Scripts': {}, 'Scripts/.config': {}, 'Scripts/.config/update_all': {}},
+        "timestamp":  int(time.time())
+    }
+
+    with open('update_all_db.json', 'w') as json_file:
+        json.dump(db, json_file, indent=4)
+
+    subprocess.run(['git', 'add', 'update_all_db.json'], check=True)
+    subprocess.run(['git', 'commit', '-m', '-'], check=True)
     subprocess.run(['git', 'push', 'origin', 'master'], check=True)
+    subprocess.run(['git', 'push', '--force', 'origin', 'db'], check=True)
+
     print("\nNew dont_download2.sh can be used.")
     with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
         f.write("NEW_RELEASE=yes\n")
@@ -24,3 +65,4 @@ else:
     print("Nothing to be updated.")
     with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
         f.write("NEW_RELEASE=no\n")
+
