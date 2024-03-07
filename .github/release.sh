17d51fdc736a463eba3d0d85c3efeee28b8c7572
José Manuel Barroso Galindo <theypsilon@gmail.com>
2024-03-07 20:29:44 +0100

    Fix

--- .github/release.sh
+++ .github/release.sh
@@ -19,6 +19,7 @@ def hash_file(path: str) -> str:
 subprocess.run(['git', 'add', 'dont_download2.sh'], check=True)
 subprocess.run(['git', 'commit', '-m', 'BOT: New dont_download2.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 subprocess.run(['git', 'fetch', 'origin', 'master'], check=True)
+subprocess.run(['cp', 'dont_download2.sh', '/tmp/dont_download2.sh'], check=True)
 
 diff_cmd = "git diff master:dont_download2.sh origin/master:dont_download2.sh"
 filter_cmd = "grep '^[+-]' | grep -v 'export COMMIT' | grep -v '^\+\+\+' | grep -v '^---'"
@@ -40,8 +41,8 @@ if int(changes) >= 1:
         "db_id": 'update_all_mister',
         "files": {
             'Scripts/.config/update_all/update_all_latest.zip': {
-                'size': os.path.getsize('dont_download2.sh'),
-                'hash': hash_file('dont_download2.sh'),
+                'size': os.path.getsize('/tmp/dont_download2.sh'),
+                'hash': hash_file('/tmp/dont_download2.sh'),
                 'url': f'https://raw.githubusercontent.com/theypsilon/Update_All_MiSTer/{commit_id}/dont_download2.sh',
                 'tags': ['updatealllatest', 'updateall']
             }
