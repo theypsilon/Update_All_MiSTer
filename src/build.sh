fed3e4530254aed90e8d03ebcc3cacebb44f6aee
José Manuel Barroso Galindo <theypsilon@gmail.com>
2024-03-07 20:40:49 +0100

    Storing update_all_latest.zip in /tmp

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-TEMP_ZIP1="$(mktemp -u).zip"
+UPDATE_ALL_ZIP="update_all_latest.zip"
 TEMP_ZIP2="$(mktemp -u).zip"
 BIN="/tmp/dont_download.zip"
 UUDECODE_CMD=$({ [[ "${MISTER:-false}" == "false" ]] && [[ "$(uname -s)" == "Darwin" ]] ; } && echo "uudecode -p" || echo "uudecode -o -")
@@ -21,11 +21,11 @@ cd src
 
 find update_all -type f -iname "*.py" -print0 | while IFS= read -r -d '' file ; do pin_metadata "${file}" ; done
 pin_metadata __main__.py
-zip -q -0 -D -X -A -r "${TEMP_ZIP1}" __main__.py update_all -x "*/__pycache__/*"
-pin_metadata "${TEMP_ZIP1}"
-echo '#!/usr/bin/env python3' | cat - "${TEMP_ZIP1}" > "${TEMP_ZIP2}"
+zip -q -0 -D -X -A -r "${UPDATE_ALL_ZIP}" __main__.py update_all -x "*/__pycache__/*"
+pin_metadata "${UPDATE_ALL_ZIP}"
+echo '#!/usr/bin/env python3' | cat - "${UPDATE_ALL_ZIP}" > "${TEMP_ZIP2}"
 pin_metadata "${TEMP_ZIP2}"
-rm "${TEMP_ZIP1}"
+mv "${UPDATE_ALL_ZIP}" "/tmp/${UPDATE_ALL_ZIP}"
 cd ..
 
 cat <<-EOF
