0a09b5cdbf9eb8c9468d5525435deae3c24a4f35
José Manuel Barroso Galindo <theypsilon@gmail.com>
2025-05-30 17:41:02 +0200

    Changing supporter plus.

--- src/update_all/constants.py
+++ src/update_all/constants.py
@@ -21,7 +21,7 @@ from enum import unique, Enum
 from typing import Final
 
 # From patreon.com/theypsilon
-supporter_plus_patrons: Final[tuple[str,...]] = ('Alex Frégeau', 'Thomas Williams')
+supporter_plus_patrons: Final[tuple[str,...]] = ('Alex Frégeau', 'Thomas Williams', "The Sentinel's Playground Team")
 
 # Default options
 DEFAULT_CURL_SSL_OPTIONS: Final[str] = ''
