f6752c3656d1fc804209fcbf9f1e1a1942016b56
José Manuel Barroso Galindo <theypsilon@gmail.com>
2026-04-07 20:18:21 +0200

    Update supporter_plus_patrons list

--- src/update_all/constants.py
+++ src/update_all/constants.py
@@ -20,7 +20,7 @@ from enum import Enum
 from typing import Final
 
 # From patreon.com/theypsilon
-supporter_plus_patrons: Final[tuple[str,...]] = ('Alex Frégeau', 'DefKonZer0', 'Thomas Williams')
+supporter_plus_patrons: Final[tuple[str,...]] = ('Alex Frégeau', 'Barry Tomkinson', 'Thomas Williams')
 
 # Default options (not in other options)
 DEFAULT_CURL_SSL_OPTIONS: Final[str] = ''
